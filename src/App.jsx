56ccf85f77c58933db95659065aa130a6e71518b
egv83 <esteban8305@ehotmail.com>
2024-04-04 22:32:09 -0500

    EVM - Se establese theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
-import { useState } from 'react'
+/* npm install antd --save */
 import { Layout } from 'antd';
 import Logo from './components/Logo';
+import MenuList from './components/MenuList';
+import ToggleThemeButton from './components/ToggleThemButton';
+import { useState } from 'react';
 
 const{Header, Sider} = Layout;
 
 function App() {
+  const [darkTheme, setDarkTheme] = useState(true);
+
+  const toggleTheme = () => {
+    setDarkTheme(!darkTheme);
+  };
 
   return (
     <Layout>
-      <Sider className='sidebar'>
+      <Sider className='sidebar' theme={darkTheme ? 'dark' : 'light'}>
         <Logo />
+        <MenuList darkTheme={darkTheme}/>
+        <ToggleThemeButton darkTheme={darkTheme} toggleTheme={toggleTheme}/>
       </Sider>
     </Layout>
   );
---
+++ src/components/MenuList.jsx
@@ -0,0 +1,43 @@
+import { Menu } from "antd";
+import {HomeOutlined, AppstoreOutlined,AreaChartOutlined,
+    PayCircleOutlined, SettingOutlined, BarsOutlined} from "@ant-design/icons";
+
+const MenuList = ({darkTheme}) => {
+    return (
+        
+        <Menu
+        theme={darkTheme ? 'dark' : 'light'}
+        mode="inline" 
+        className="menu-bar">
+
+            <Menu.Item key="home" icon={<HomeOutlined />}>
+                Home
+            </Menu.Item>
+            <Menu.Item key="activity" icon={<AppstoreOutlined />}>
+                Activity
+            </Menu.Item>
+            <Menu.SubMenu key="tasks" icon={<BarsOutlined />}>
+                <Menu.Item key="task-1">Task 1</Menu.Item>
+                <Menu.Item key="task-2">Task 2</Menu.Item>
+
+                <Menu.SubMenu key="subtasks" title="Subtasks">
+                    <Menu.Item key="subTask-1">Subtask-1</Menu.Item>
+                    <Menu.Item key="subTask-2">Subtask-2</Menu.Item>
+                </Menu.SubMenu>
+
+            </Menu.SubMenu>
+            <Menu.Item key="progress" icon={<AreaChartOutlined />}>
+                Progress
+            </Menu.Item>
+            <Menu.Item key="payment" icon={<PayCircleOutlined />}>
+                Payment
+            </Menu.Item>
+            <Menu.Item key="sttings" icon={<SettingOutlined />}>
+                Settings
+            </Menu.Item>
+
+        </Menu>
+    );
+}
+
+export default MenuList;
