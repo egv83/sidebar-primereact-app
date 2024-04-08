7d63b397956a7343ed03dffd8f18cd7b1c93d978
egv83 <esteban8305@ehotmail.com>
2024-04-07 19:07:36 -0500

    EVM - sidebar terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,47 @@
 /* npm install antd --save */
-import { Layout } from 'antd';
+import { useState } from 'react';
+import {Button, Layout, theme } from 'antd';
+import {MenuUnfoldOutlined, MenuFoldOutlined} from '@ant-design/icons'
 import Logo from './components/Logo';
 import MenuList from './components/MenuList';
 import ToggleThemeButton from './components/ToggleThemButton';
-import { useState } from 'react';
+
 
 const{Header, Sider} = Layout;
 
 function App() {
   const [darkTheme, setDarkTheme] = useState(true);
+  const [collapsed, setCollapsed] = useState(false);
 
   const toggleTheme = () => {
     setDarkTheme(!darkTheme);
   };
 
+  const {
+    token: {colorBgContainer},
+  } = theme.useToken();
+
   return (
     <Layout>
-      <Sider className='sidebar' theme={darkTheme ? 'dark' : 'light'}>
+      <Sider className='sidebar' 
+      collapsed = {collapsed}
+      collapsible
+      trigger={null}
+      theme = {darkTheme ? 'dark' : 'light'}>
         <Logo />
         <MenuList darkTheme={darkTheme}/>
         <ToggleThemeButton darkTheme={darkTheme} toggleTheme={toggleTheme}/>
       </Sider>
+      <Layout>
+        <Header style={{padding: 0, background:
+          colorBgContainer}}>
+          <Button type='text' 
+          className='toggle'
+          onClick={() => setCollapsed(!collapsed)}
+          icon={collapsed ? 
+          <MenuUnfoldOutlined /> : <MenuFoldOutlined />  }/>
+        </Header>
+      </Layout>
     </Layout>
   );
 }
