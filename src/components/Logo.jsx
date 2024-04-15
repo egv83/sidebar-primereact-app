2dfdb10249c86c937f1def684d5e515e2a2af89f
egv83 <esteban8305@ehotmail.com>
2024-04-14 20:12:04 -0500

    EVM - se añade logo 8bit

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import {FireFilled} from '@ant-design/icons'
+import logo from '../assets/img/LOGO_8BITS.png'
 
 const Logo = ( )=> {
     return (
         <div className="logo">
             <div className="logo-icon">
-                <FireFilled />
+                {/* <FireFilled /> */}
+                <img className="tiny-logo" src={logo}></img>
             </div>
         </div>
     )
